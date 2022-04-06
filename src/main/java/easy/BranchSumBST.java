f1b8d2da99e8a7c020a6bec38093ea12c00fa430
Jooah Seo <seo.jooah@gmail.com>
2022-04-06 18:59:17 -0400

    add enhanced version

--- src/main/java/easy/BranchSumBST.java
+++ src/main/java/easy/BranchSumBST.java
@@ -25,6 +25,7 @@ public class BranchSumBST {
         ArrayList<Integer> sum = new ArrayList<>();
         return addSum(root, 0, sum);
     }
+
     public static List<Integer> addSum(BinaryTree currentNode, int currentSum, ArrayList<Integer> sum){
         currentSum = currentSum + currentNode.value;
         if(currentNode.left != null){
@@ -37,4 +38,27 @@ public class BranchSumBST {
 
         return sum;
     }
+
+    /**
+     * Write enhanced version
+     */
+    public static List<Integer> branchSums2(BinaryTree root) {
+        // Write your code here.
+        ArrayList<Integer> sum = new ArrayList<>();
+        addSum2(root, 0, sum);
+        return sum;
+    }
+
+    public static void addSum2(BinaryTree currentNode, int currentSum, ArrayList<Integer> sum){
+        if(currentNode == null) return ;
+        currentSum += currentNode.value;
+
+        if(currentNode.left == null && currentNode.right == null){
+            sum.add(currentSum);
+            return ;
+        }
+        addSum2(currentNode.left, currentSum, sum);
+        addSum2(currentNode.right, currentSum, sum);
+    }
+
 }
