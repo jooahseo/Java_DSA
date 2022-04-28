cf3b3392cd3800320775b4b5eb02d3629ecd5796
Jooah Seo <seo.jooah@gmail.com>
2022-04-28 08:06:53 -0400

    add O(n) time solution

--- src/main/java/easy/FirstNonRepeatingChar.java
+++ src/main/java/easy/FirstNonRepeatingChar.java
@@ -1,11 +1,12 @@
 package easy;
 
-import java.util.HashSet;
+import java.util.Hashtable;
 
 public class FirstNonRepeatingChar {
     public static void main(String[] args) {
         String string = "abcdcaf";
         System.out.println(firstNonRepeatingCharacter(string)); // should return 1
+        System.out.println(firstNonRepeatingCharacter2(string)); // should return 1
     }
 
     /**
@@ -22,6 +23,26 @@ public class FirstNonRepeatingChar {
                 }
             }
             if(!repeating) return i;
+        }
+        return -1;
+    }
+
+    /**
+     * Optimized in time
+     * O(n) Time, O(n) space
+     */
+    public static int firstNonRepeatingCharacter2(String string) {
+        Hashtable<Character, Integer> freqTable = new Hashtable<>();
+        for(int i=0; i<string.length(); i++){
+            char currChar = string.charAt(i);
+            if(freqTable.containsKey(currChar)){
+                freqTable.put(currChar, freqTable.get(currChar) + 1);
+            }else{
+                freqTable.put(currChar, 1);
+            }
+        }
+        for(int i=0; i<string.length(); i++){
+            if(freqTable.get(string.charAt(i)) == 1) return i;
         }
         return -1;
     }
