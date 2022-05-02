3a5d9bbec70982379f46a3867611a93279404dc5
Jooah Seo <seo.jooah@gmail.com>
2022-05-02 08:06:27 -0400

    add solution to smallest difference - brute force way

---
+++ src/main/java/medium/SmallestDifference.java
@@ -0,0 +1,50 @@
+package medium;
+
+/**
+ * Write a function that takes in two non-empty arrays of integers, finds the pair of numbers(one from each array)
+ * whose absolute difference is closest to zero, and returns an array containing these two numbers,
+ * with the number from the first array in the first position
+ *
+ * The absolute difference of -5 and 5 is 10, and the absolute difference of -5 and -4 is 1.
+ * There will be only 1 pair of numbers with the smallest difference.
+ */
+public class SmallestDifference {
+    public static void main(String[] args) {
+        int[] arrayOne = {-1, 5, 10, 2, 28, 3};
+        int[] arrayTwo = {26, 134, 135, 15, 17};
+
+        for(int n : smallestDifference(arrayOne, arrayTwo)){
+            System.out.println(n); //print 28, 26
+        }
+    }
+
+    /**
+     * Brute force approach - O(n^2) Time, O(1) space
+     */
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+        int[] smallestDiffPair = new int[2];
+        int smallestDifference = Integer.MAX_VALUE;
+        for(int i=0; i<arrayOne.length; i++){
+            int numFromFirst = arrayOne[i];
+            for(int j=0; j<arrayTwo.length; j++){
+                int currDiff = Math.abs(numFromFirst - arrayTwo[j]);
+                if(currDiff < smallestDifference){
+                    smallestDiffPair[0] = numFromFirst;
+                    smallestDiffPair[1] = arrayTwo[j];
+                    smallestDifference = currDiff;
+                }
+            }
+        }
+        return smallestDiffPair;
+    }
+
+    /**
+     * TODO: Enhanced approach
+     */
+    public static int[] smallestDifference2(int[] arrayOne, int[] arrayTwo) {
+        int[] smallestDiffPair = new int[2];
+        int smallestDifference = Integer.MAX_VALUE;
+
+        return smallestDiffPair;
+    }
+}
