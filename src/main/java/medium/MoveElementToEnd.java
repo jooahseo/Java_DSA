6a968eb0f10f55eb9fa92fe7ba330b8294b05c12
Jooah Seo <seo.jooah@gmail.com>
2022-05-04 07:20:22 -0400

    add refactored version of solution

--- src/main/java/medium/MoveElementToEnd.java
+++ src/main/java/medium/MoveElementToEnd.java
@@ -14,7 +14,7 @@ public class MoveElementToEnd {
         List<Integer> array = Arrays.asList(2, 1, 2, 2, 2, 3, 4, 2);
         int toMove = 2;
 
-        moveElementToEnd(array, toMove);
+        moveElementToEnd2(array, toMove);
         for(int n : array){
             System.out.print(n + ", ");
             //expect to print 1, 3, 4, 2, 2, 2, 2, 2, -> the number 1,3,4 could be ordered differently
@@ -24,7 +24,7 @@ public class MoveElementToEnd {
         List<Integer> array2 = Arrays.asList(1, 2, 4, 5, 6);
         int toMove2 = 3;
 
-        moveElementToEnd(array2, toMove2);
+        moveElementToEnd2(array2, toMove2);
         for(int n : array2){
             System.out.print(n + ", ");
             //expect to print 1, 2, 4, 5, 6
@@ -53,5 +53,26 @@ public class MoveElementToEnd {
             array.set(right, temp);
         }
         return array;
+    }
+
+    /**
+     * Refactored version O(n) Time, O(1) Space
+     */
+    public static List<Integer> moveElementToEnd2(List<Integer> array, int toMove) {
+        int left = 0;
+        int right = array.size()-1;
+
+        while(left < right){
+            while(left < right && array.get(right) == toMove) right --;
+            if(array.get(left) == toMove) swap(array, left, right);
+            left ++;
+        }
+        return array;
+    }
+
+    public static void swap(List<Integer> array, int left, int right){
+        int temp = array.get(left);
+        array.set(left, array.get(right));
+        array.set(right, temp);
     }
 }
